9f2a11fd255777f1d1a7670dbe918e7b95e8e034
Hassan Falahi <hfalahi@gmail.com>
2019-06-17 11:20:27 +0430

    fix

--- src/containers/customer.js
+++ src/containers/customer.js
@@ -1,80 +1,37 @@
-import React, { useState, useEffect, useContext } from "react";
-import { format as fnsFormat, parse as fnsParse } from "date-fns";
+import React from "react";
 import {WhiteSpace, Card, CardItem, CardItemRow } from "../components/base/misc";
-import { Button, Input } from "../components/base/forms";
-import api from "../api/api";
 import vihicleImage from '../images/vihicleImage.png'
-const Home = () => {
-  const name = useInputForm("");
-
-  useDocumentTitle(name.value);
-  return (
-    <div  >
-      <div className="header">
-        <h1 className="header__title"> Connected Vehicles</h1>
-      </div>
-      <div className="container">
-        <Card title="Customer Information">
-
-        </Card>
-        <WhiteSpace />
-
-        <Card title="Vehicles">
-          <CardItem>
-            <img style={{width:'100px'}} src={vihicleImage} />
-            <CardItemRow label="VIN" value="YS2R4X9401"></CardItemRow>
-            <CardItemRow label="Reg No" value="Johans Bulk AB"></CardItemRow>
-            <CardItemRow label="Customer" value="YS2R4X9401"></CardItemRow>
-            <CardItemRow label="Status" status="disconnected" value="Connected"></CardItemRow>
-          </CardItem>
-        </Card>
-      </div>
-    </div>
-  );
-};
-
-const useInputForm = (initialValue, callback) => {
-  const [value, setValue] = useState(initialValue);
-
-  function onChange(e) {
-    setValue(e.target.value);
-    callback();
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
   }
-  return { value, onChange };
-};
 
-const useWindowWith = () => {
-  const [width, setWidth] = useState(window.innerWidth);
-  useEffect(() => {
-    const handleResize = () => {
-      setWidth(window.innerWidth);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  });
-  return width;
-};
+  render() {
 
-const useDocumentTitle = value => {
-  useEffect(() => {
-    document.title = value;
-  });
-};
+    return (
+      <div  >
+        <div className="header">
+          <h1 className="header__title"> Connected Vehicles</h1>
+        </div>
+        <div className="container">
+          <Card title="Customer Information">
 
-const useFetch = method => {
-  const [result, setResult] = useState([]);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    async function fetchData() {
-      const res = await api[method]();
-      setResult(res.data);
-      setLoading(false);
-    }
-    fetchData();
-  }, []);
-  return { result, loading };
-};
+          </Card>
+          <WhiteSpace />
+
+          <Card title="Vehicles">
+            <CardItem>
+              <img style={{width:'100px'}} src={vihicleImage} />
+              <CardItemRow label="VIN" value="YS2R4X9401"></CardItemRow>
+              <CardItemRow label="Reg No" value="Johans Bulk AB"></CardItemRow>
+              <CardItemRow label="Customer" value="YS2R4X9401"></CardItemRow>
+              <CardItemRow label="Status" status="disconnected" value="Connected"></CardItemRow>
+            </CardItem>
+          </Card>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Home;
