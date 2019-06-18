fb7bdad106b0b105a96297c1d5016c2a01301875
Hassan Falahi <hfalahi@gmail.com>
2019-06-18 11:54:17 +0430

    github pages

--- src/utils/config/production.js
+++ src/utils/config/production.js
@@ -1,6 +1,6 @@
 const production = {
   apiTimeout: 30000,
-  apiEndpoint: "http://localhost:8080/v1/",
+  apiEndpoint: "https://connected-vehicles.herokuapp.com/v1/",
   version: "v1"
 };
 export default production;
