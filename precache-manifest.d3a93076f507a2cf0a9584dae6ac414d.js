e9811a8d982483e5f4172f02af03e8ee66c19d2d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-18 09:03:12 +0000

    Deploy peekbyte/connected-vehicles-front to github.com/peekbyte/connected-vehicles-front.git:gh-pages

---
+++ precache-manifest.d3a93076f507a2cf0a9584dae6ac414d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd97737c2f8b2ace4591df250dc5801",
+    "url": "/connected-vehicles-front/index.html"
+  },
+  {
+    "revision": "525c4bb33fd222a6a0f9",
+    "url": "/connected-vehicles-front/static/css/2.06d61af9.chunk.css"
+  },
+  {
+    "revision": "e680ecfd37dbf77a8018",
+    "url": "/connected-vehicles-front/static/css/main.15f15922.chunk.css"
+  },
+  {
+    "revision": "525c4bb33fd222a6a0f9",
+    "url": "/connected-vehicles-front/static/js/2.ccd5697e.chunk.js"
+  },
+  {
+    "revision": "e680ecfd37dbf77a8018",
+    "url": "/connected-vehicles-front/static/js/main.dd0bf9e7.chunk.js"
+  },
+  {
+    "revision": "8cea34598e17e3244e18",
+    "url": "/connected-vehicles-front/static/js/runtime~main.9fbc5d8d.js"
+  },
+  {
+    "revision": "dd65cf74720c805794daa796a2bad852",
+    "url": "/connected-vehicles-front/static/media/Gear-1s-200px.dd65cf74.gif"
+  },
+  {
+    "revision": "86c33bf9f7d7a7743aa6604ccd669387",
+    "url": "/connected-vehicles-front/static/media/vihicleImage.86c33bf9.png"
+  }
+]);
