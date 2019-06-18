3ea7f28d838436256c0007aa62d16cfa4a32c58e
Hassan Falahi <hfalahi@gmail.com>
2019-06-18 12:04:56 +0430

    exclude build folder

---
+++ build/precache-manifest.c58c399bf13e4cbb9f949458b72bc484.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d10c87405a23b24208d4fd0d233135",
+    "url": "/connected-vehicles-front/index.html"
+  },
+  {
+    "revision": "bf0d0b5fa7c171907ab3",
+    "url": "/connected-vehicles-front/static/css/2.7c9e9833.chunk.css"
+  },
+  {
+    "revision": "7d975fa2a5db5b4eba8b",
+    "url": "/connected-vehicles-front/static/css/main.4aff8b44.chunk.css"
+  },
+  {
+    "revision": "bf0d0b5fa7c171907ab3",
+    "url": "/connected-vehicles-front/static/js/2.ccd5697e.chunk.js"
+  },
+  {
+    "revision": "7d975fa2a5db5b4eba8b",
+    "url": "/connected-vehicles-front/static/js/main.99b5b7c6.chunk.js"
+  },
+  {
+    "revision": "8cea34598e17e3244e18",
+    "url": "/connected-vehicles-front/static/js/runtime~main.9fbc5d8d.js"
+  },
+  {
+    "revision": "dd65cf74720c805794daa796a2bad852",
+    "url": "/connected-vehicles-front/static/media/Gear-1s-200px.dd65cf74.gif"
+  },
+  {
+    "revision": "86c33bf9f7d7a7743aa6604ccd669387",
+    "url": "/connected-vehicles-front/static/media/vihicleImage.86c33bf9.png"
+  }
+]);
